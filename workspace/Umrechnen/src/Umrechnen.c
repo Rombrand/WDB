1738b02e9c34378f86aa88f3dbb797267fe3f562
Christopher Wendt <christopher.wendt@seneos.com>
2017-06-27 11:24:25 +0200

    remove work

---
+++ workspace/Umrechnen/src/Umrechnen.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : Umrechnen.c
+ Author      : Christopher Wendt
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	const int umrech = 273;
+	int merker;
+
+		while(1)
+		{
+			printf ("Geben Sie eine Temeratur in Grad-Kelvin ein: ");
+
+			scanf("%d", &merker);
+
+			printf("%d Grad-Kelvin = %d Grad-Celsius\n",merker , merker-umrech);
+
+		}
+
+	return 0;
+}
